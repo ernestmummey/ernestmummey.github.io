0bab544ff24ab805d9468d7d6e38455c2b50e05e
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-06-18 21:55:27 -0400

    Created components, Added and assests folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Image from 'material-ui-image'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Header from './components/Header';
+import Landing from './components/Landing';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -12,25 +10,12 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button'
-import { findByLabelText } from '@testing-library/react';
+import { CssBaseline } from '@material-ui/core';
+
 
 const useStyles = makeStyles((theme) => ({
-  appbar: {
-      backgroundColor: "#00664d"
-  },
-  hero: {
-      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://images.unsplash.com/photo-1619484830529-b06123fbdbd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=80")`,
-      height: "1000px",
-      backgroundPosition: "center",
-      backgroungRepeat: "no-repeat",
-      backgroundSize: "cover",
-      position: "relative", 
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      color: "#fff",
-      fontSize: "6rem",
-  },
+
+
   aboutContainer: {
     paddingTop: theme.spacing(3),
   },
@@ -52,16 +37,9 @@ function App() {
   const classes = useStyles();
   return (
     <div className="App">
-        <AppBar position ="fixed" className = {classes.appbar}>
-                <Toolbar>
-                    <Typography variant= "h4" color= " primary">
-                        Ernest Mummey
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Box className={classes.hero}>
-                <Box>Under Construction</Box>
-            </Box>
+      <CssBaseline/>
+      <Header/>
+      <Landing/>
             <Container className={classes.aboutContainer}>
               <Typography variant="h2">About Me</Typography>
               <img className = {classes.image} src="https://dummyimage.com/300x300/000/fff&text=Placeholder"/>
