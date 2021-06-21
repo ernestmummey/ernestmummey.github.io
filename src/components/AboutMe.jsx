52668454555d339fad98e8b2d0a89f2e6c2e1de6
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-06-20 21:45:20 -0400

    Added a shadow for better seperation

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 
@@ -21,23 +22,26 @@ const useStyles = makeStyles(() => ({
     },
 
 }))
+
 export default function AboutMe(){
     const classes = useStyles();
     
     return (
-        <div className = {classes.root}>
+        <div>
+            <Box className = {classes.root} boxShadow={3}>
             <div>
                 <h1> About Me </h1>  
                 <h3>Brief</h3> 
             </div>
             <div>
-                <Avatar className = {classes.avatarPic} alt="Picture of myself with the pups" src="/assets/aboutme.jpeg" />
+                <Avatar className={classes.avatarPic} alt="Picture of myself with the pups" src="/assets/aboutme.jpeg" />
             </div>
             <div>
                 <Typography variant="body1" >
                         Storage shed, troughs feed bale manure, is garden wheat oats at augers. Bulls at rose garden cucumbers mice sunflower wheat in pig. Chainsaw foal hay hook, herbs at combine harvester, children is mallet. Goat goose hen horse. Pick up truck livestock, pets and storage shed, troughs feed bale manure, is garden wheat oats at augers. Lamb. Storage shed, troughs feed bale manure, is garden wheat oats at augers. Bulls at rose garden cucumbers mice sunflower wheat in pig. Chainsaw foal hay hook, herbs at combine harvester, children is mallet. Goat goose hen horse. Pick up truck livestock, pets and storage shed, troughs feed bale manure, is garden wheat oats at augers. Lamb.
                 </Typography>
             </div>
+            </Box>
         </div>
     )
 }
