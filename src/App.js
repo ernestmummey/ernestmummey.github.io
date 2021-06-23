acedeaa45994c6bb40d36246de497194dfadc13d
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-06-23 19:42:50 -0400

    Changed contact to connect

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Header from './components/Header';
 import Landing from './components/Landing';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Connect from './components/Connect';
 import { CssBaseline } from '@material-ui/core';
+
 
 
 const useStyles = makeStyles((theme) => ({}));
@@ -19,7 +20,7 @@ function App() {
       <Landing/>
       <AboutMe/>
       <Projects/>
-      <Contact/>
+      <Connect/>
     </div>
   );
 }
