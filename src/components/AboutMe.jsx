612af464d9e7c015e81dfbe4c8baa1224462472f
Ernest Mummey <ernest.j.mummey@gmail.com>
2021-07-22 20:15:04 -0400

    added paragraph

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -34,6 +34,8 @@ const useStyles = makeStyles(() => ({
         color: "#F8F8FF",
     },
     body: {
+        marginLeft: '20px',
+        marginRight:'20px',
         fontFamily: "Nunito",
         color: "#F8F8FF",
         fontSize: "2rem",
@@ -58,8 +60,10 @@ export default function AboutMe(){
                 <Avatar className={classes.avatarPic} alt="Picture of myself with the pups" src="/assets/aboutme.jpeg" />
             </div>
             <div>
-                <Typography className={classes.body} variant="body1" >
-                Hello! I'm Ernest, a full stack engineer
+                <Typography className={classes.body} variant="body1" >  
+                Growing up I loved to see how things worked. As a kid I took apart my mother’s appliances to include her toaster and microwave trying to figure out how they operated or if I could repair it. This mentality eventually led me to put together my first vehicle in high school. After serving 5 years in the military, I realized I no longer enjoyed what I was doing, and I knew it was time to change. I then found programming. My curiosity drove me to wanting to understand more, soon I was reading  and watching videos about it daily. I quickly realized that I found my passion again. 
+                <br/>
+                Programming allows me to go beyond my imagination, add creativity, and truly let me dive into understanding how everything works. I took a leap of faith and decided to go all in and join Coding Dojo bootcamp. There I was able to work in teams and collaborate with my fellow classmates and be able to help others. This bootcamp taught me 3 full stacks in three months. These stacks include Python, MERN, Java, allowing me to build several projects using different technologies.
                 </Typography>
             </div>
         </div>
