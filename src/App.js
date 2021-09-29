5b884274c8abacdacf85d25a646bdfb5de9b7308
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-09-29 12:17:37 -0400

    Updating the porfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Header from './components/Header';
-import Landing from './components/Landing';
-import AboutMe from './components/AboutMe';
-import Projects from './components/Projects';
-import Connect from './components/Connect';
-import { CssBaseline } from '@material-ui/core';
+import Landing from './Views/Landing.jsx'
 
 
 
-const useStyles = makeStyles((theme) => ({}));
 
 function App() {
-  const classes = useStyles();
+  
   return (
-    <div className = {classes.root}>
-      <CssBaseline/>
-      <Header/>
+    <div>
       <Landing/>
-      <AboutMe/>
-      <Projects/>
-      <Connect/>
     </div>
   );
 }
---
+++ src/Views/Landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Style/Landing.scss'
+
+
+
+const Landing = (props) => {
+    return (
+        <div className ='container'>
+            <div className="introduction">
+                <h1>Ernest Mummey</h1>
+                    <div className="intrduction__job">
+                        <h4>Full Stack Engineer</h4>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
+
+
