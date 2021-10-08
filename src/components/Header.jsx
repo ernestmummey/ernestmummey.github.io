79c837fc338339b58cb5b1ac097defc126030f54
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-10-08 19:26:20 -0400

    Created a list class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,18 @@ const Header = () => {
     return (
         <div className="header__wrapper">
             <div className='header__container'>
-                <div className="logo">
-                    <p>
-                        logo
-                    </p>
-                </div>
-                <ul>
-                    <li>About Me</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
+                    <div className="logo">
+                        <p>
+                            logo
+                        </p>
+                    </div>
+                    <div className="list">
+                        <ul>
+                            <li>About Me</li>
+                            <li>Projects</li>
+                            <li>Contact</li>
+                        </ul> 
+                    </div>
             </div>
         </div>
     )
