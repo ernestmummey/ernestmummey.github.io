c3a7771ab7cab8f495884ff9cf3e72c34ebf79a7
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-10-09 13:18:28 -0400

    created project

---
+++ src/Views/Project.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Project = () => {
+    return (
+        <div className="project__container">
+            <div>Turtle</div>
+            <div>Turtle</div>
+            <div>Turtle</div>
+            <div>Turtle</div>
+            <div>Turtle</div>
+        </div>
+    )
+};
+
+export default Project;
