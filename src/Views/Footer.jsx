5e055ae5512fdc4ccce673bb903925b87cd5724e
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-10-17 15:39:22 -0400

    Created a footer

---
+++ src/Views/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Style/Footer.scss'
+
+
+const Footer = () => {
+
+    return (
+        <div className="footer__container">
+            <div className="footer__wrapper">
+                <div className="images"></div>
+                <div className="images"></div>
+                <div className="images"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
